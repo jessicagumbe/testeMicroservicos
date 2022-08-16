72f871168c7c40e22293726928716e7d67277782
jessicagumbe <65067449+jessicagumbe@users.noreply.github.com>
2022-08-16 11:00:30 +0100

    criação do projecto

---
+++ src/main/java/com/testemicroservicos/TesteMicroservicosApplication.java
@@ -0,0 +1,13 @@
+package com.testemicroservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteMicroservicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteMicroservicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testemicroservicos/TesteMicroservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testemicroservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteMicroservicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
